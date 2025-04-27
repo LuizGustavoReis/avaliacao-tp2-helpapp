6d5a77f0ad8991698026e352be4c501d925d3818
LuizGustavoReis <luizgusreis@gmail.com>
2025-04-27 13:36:42 -0300

    feito tudo com muito carinho

--- HelpApp/HelpApp.Infra.Data/Repositories/CategoryRepository.cs
+++ HelpApp/HelpApp.Infra.Data/Repositories/CategoryRepository.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HelpApp.Domain.Entities;
+using HelpApp.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace HelpApp.Infra.Data.Repositories
 {
-    public class CategoryRepository
+    public class CategoryRepository : ICategoryRepository
     {
+        private readonly DbContext _context;
+
+        public CategoryRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            return await _context.Set<Category>().ToListAsync();
+        }
+
+        public async Task<Category> GetById(int? id)
+        {
+            return await _context.Set<Category>().FindAsync(id);
+        }
+
+        public async Task<Category> Create(Category category)
+        {
+            await _context.Set<Category>().AddAsync(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> Update(Category category)
+        {
+            _context.Set<Category>().Update(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> Remove(Category category)
+        {
+            _context.Set<Category>().Remove(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
     }
 }
